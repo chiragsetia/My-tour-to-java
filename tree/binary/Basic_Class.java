6f615b67dcd795ce126df711e404db04b2c3c332
Chirag Setia <chiragsetia0.cs@gmail.com>
2019-06-13 10:10:39 +0530

    Create Basic_Class.java

---
+++ tree/binary/Basic_Class.java
@@ -0,0 +1,10 @@
+/*	Binary Tree Node class*/
+ class BinaryTreeNode<T> {
+		T data;
+		BinaryTreeNode<T> left;
+		BinaryTreeNode<T> right;
+		
+		public BinaryTreeNode(T data) {
+			this.data = data;
+		}
+	}
